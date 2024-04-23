e3fcb5dadb5130a38b02e7322e2d4a042a57da9f
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2024-04-23 12:01:07 -0300

    Mais teste com Animal

--- src/Avore/model/Main.java
+++ src/Avore/model/Main.java
@@ -1,5 +1,6 @@
 package Avore.model;
 
+import Animal.*;
 public class Main {
     public static void main(String[] args) {
         Avl<Integer> arvore = new Avl<Integer>();
@@ -8,5 +9,12 @@ public class Main {
         for(Integer i : ints) arvore.inserir(i);
 
         arvore.imprimir();
+
+        Animal animal = new Animal(01, 'F', "Laila", "Cachorro", "10-10-10", "20-03-24");
+        animal.addMonitoramento(new Monitoramento(5.0, 0.50, 25.0, true, false, "Muito magra", "23-04-24"));
+
+        animal.addMonitoramento(new Monitoramento(5.0, 0.50, 25.0, true, false, "Muito magra", "25-04-24"));
+
+        animal.imprimirHistorico();
     }
 }
